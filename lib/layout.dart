5fd6c8a23bf002ee20c467e0498718f02dc9fb49
part-time-indus <epic_indus_dev@gmx.com>
2022-10-25 02:35:55 +1100

    Title widget created.

---
+++ lib/layout.dart
@@ -0,0 +1,45 @@
+//TODO 1: Column Widget composed of Image, title row, button row and text
+  //TODO 1A: Title Widget composed of Row Widget
+    //TODO 1AA: Row widget composed of Column Widget and Row Widget
+      //TODO 1AAA: Column Widget composed of Place Name (Bold Title) and it's Location
+      //TODO 1AAB: Row Widget composed of fav icon and rating (int)
+  //TODO 1B: Button Widget composed of Row Widget
+    //TODO 1BA: Row Widget composed of 3 Column Widgets
+      //TODO 1BAA: Column Widets composed of Icon and Text
+  //TODO 1C: Text Widget 
+
+  import 'package:flutter/material.dart';
+
+  class Screen extends StatelessWidget{
+    const Screen({Key? key}): super(key: key);
+    @override
+    Widget build(BuildContext context) {
+      return Container(
+        child: Column(
+          children: [title(),]
+        ),
+      );    
+    }
+
+    Widget title(){
+      return Row(children: [name(), rating()],);
+    }
+
+    Widget rating(){
+      return Row(
+        children: <Widget>[
+          IconButton(onPressed: (){}, icon: const Icon(Icons.star),),
+          const Text('41')
+        ],
+      );
+    }
+
+    Widget name(){
+      return Column(
+        children: const <Widget>[
+          Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold),),
+          Text ('Kandersteg, Switzerland'),
+        ],
+      );
+    }
+  }
